bfc00806b3083ed741bd42c659ec39b1416b3290
Charles Hill <hill3cj@mail.uc.edu>
2019-05-24 00:41:30 -0400

    Added a modified blazor front-end for the new django backend
    Signed-off-by: Charles Hill <hill3cj@mail.uc.edu>

---
+++ BlazorWebClient/BlazorClient/Shared/BetterJSONExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Shared
+{
+    /// <summary>
+    /// A copy of the HttpClientJsonExtensions, but made better by using Newtonsoft.Json instead of the JSInterop.Json classes
+    /// </summary>
+    public static class BetterJSONExtensions
+    {
+        public static async Task<T> GetJsonBetterAsync<T>(this HttpClient httpClient, string requestUri)
+        {
+            var responseJson = await httpClient.GetStringAsync(requestUri);
+            return JsonConvert.DeserializeObject<T>(responseJson);
+        }
+
+        public static async Task PostJsonBetterAsync(this HttpClient httpClient, string requestUri, object content)
+            => await httpClient.SendJsonBetterAsync(HttpMethod.Post, requestUri, content);
+
+        public static async Task<T> PostJsonBetterAsync<T>(this HttpClient httpClient, string requestUri, object content)
+            => await httpClient.SendJsonBetterAsync<T>(HttpMethod.Post, requestUri, content);
+
+        public static async Task PutJsonBetterAsync(this HttpClient httpClient, string requestUri, object content)
+            => await httpClient.SendJsonBetterAsync(HttpMethod.Put, requestUri, content);
+
+        public static async Task<T> PutJsonBetterAsync<T>(this HttpClient httpClient, string requestUri, object content)
+            => await httpClient.SendJsonBetterAsync<T>(HttpMethod.Put, requestUri, content);
+
+        public static Task SendJsonBetterAsync(this HttpClient httpClient, HttpMethod method, string requestUri, object content)
+            => httpClient.SendJsonBetterAsync<IgnoreResponse>(method, requestUri, content);
+
+        public static async Task<T> SendJsonBetterAsync<T>(this HttpClient httpClient, HttpMethod method, string requestUri, object content)
+        {
+            var requestJson = JsonConvert.SerializeObject(content);
+            var response = await httpClient.SendAsync(new HttpRequestMessage(method, requestUri)
+            {
+                Content = new StringContent(requestJson, Encoding.UTF8, "application/json")
+            });
+
+            // Make sure the call was successful before we
+            // attempt to process the response content
+            response.EnsureSuccessStatusCode();
+
+            if (typeof(T) == typeof(IgnoreResponse))
+            {
+                return default;
+            }
+            else
+            {
+                var responseJson = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(responseJson);
+            }
+        }
+
+        class IgnoreResponse {  }
+    }
+}
---
+++ BlazorWebClient/BlazorClient/Shared/Student.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorClient.Shared
+{
+    public class Student
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("m_number")]
+        [RegularExpression(@"M\d{8}", ErrorMessage = "Please include an M followed by 8 digits")]
+        public string MNumber { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+    }
+}

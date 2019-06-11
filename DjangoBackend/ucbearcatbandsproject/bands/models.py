3c5d3eca825bd4a975f039b2243efbdd85428a5d
Charles Hill <hill3cj@mail.uc.edu>
2019-06-11 15:45:55 -0400

    Temporarily de-coupled Users from Student objects to keep development going.

--- DjangoBackend/ucbearcatbandsproject/bands/models.py
+++ DjangoBackend/ucbearcatbandsproject/bands/models.py
@@ -1,18 +1,12 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from ..settings import AUTH_USER_MODEL
-
 
 # Create your models here.
-
-
 class Student(models.Model):
-    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
-
     m_number = models.CharField(max_length=10,
                                 validators=[RegexValidator(
                                     regex=r'[Mm]\d{8,8}',
                                     message='Please enter an M followed by 8 digits')])
 
     def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+        return '{}'.format(self.m_number)

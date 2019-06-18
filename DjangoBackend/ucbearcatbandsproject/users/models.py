80b3c6ebbfab91309f522684a137450fe8834ed8
Charles Hill <hill3cj@mail.uc.edu>
2019-06-18 18:12:33 -0400

    The User object now has a general "Full Name" field

--- DjangoBackend/ucbearcatbandsproject/users/models.py
+++ DjangoBackend/ucbearcatbandsproject/users/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+# Ideally we'll update this user down the line if we need to make changes
 class CustomUser(AbstractUser):
     is_student = models.BooleanField(default=True)
+    full_name = models.CharField(blank=False, max_length=255, default='Noname Jones')
 
     def __str__(self):
         return self.email
-        

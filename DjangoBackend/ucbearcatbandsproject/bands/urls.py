1637c2f2e0adba43a72f6c3152006c20be9e796d
Charles Hill <hill3cj@mail.uc.edu>
2019-07-01 21:50:52 -0400

    Fixed typo 8)

--- DjangoBackend/ucbearcatbandsproject/bands/urls.py
+++ DjangoBackend/ucbearcatbandsproject/bands/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from ..users.views import UserViewSet
 
-
 # Create a router and register our viewsets in it
 router = DefaultRouter()
 router.register(r'students', views.StudentViewSet)
@@ -12,7 +11,7 @@ router.register(r'instruments', views.InstrumentViewSet)
 router.register(r'users', UserViewSet)
 router.register(r'uniforms', views.UniformViewSet)
 
-# Include the viewset's urls, as well as our authentication urls
+# Include the viewsets' urls, as well as our authentication urls
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),

f2913b438e194642633f1a3c4c701a82b12e3851
Stephanie Tam <stephtam8@gmail.com>
2019-07-16 19:15:16 -0400

    Tried to add hamburger menu
    i hope

--- MobileApp/MobileApp/View Controllers/AdminPortalVC.swift
+++ MobileApp/MobileApp/View Controllers/AdminPortalVC.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class AdminPortalVC: UIViewController {
+    var menuShow = false
+    
+    @IBOutlet weak var LeadingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         let db = SampleDatabasePopulation()
         db.populate()
     }
     
+    @IBAction func openMenu(_ sender: Any) {
+    if (menuShow) {
+        LeadingConstraint.constant = -150
+    }
+    else {
+        LeadingConstraint.constant = 0
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    menuShow = !menuShow
+    }
+
 }

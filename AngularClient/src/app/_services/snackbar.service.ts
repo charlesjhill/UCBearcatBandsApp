78992352fb8149ef136336c35e87fe09e7a7bb38
Charles Hill <hill3cj@mail.uc.edu>
2019-07-30 01:54:16 -0400

    Added a more "dangerous" snackbar

--- AngularClient/src/app/_services/snackbar.service.ts
+++ AngularClient/src/app/_services/snackbar.service.ts
@@ -15,4 +15,13 @@ export class SnackBarService {
       duration
     });
   }
+
+  public openDeleteSnackBar(message: string, action?: string, duration?: number): MatSnackBarRef<any> {
+    duration = duration || 2000;
+    action = action || '';
+    return this.snackbar.open(message, action, {
+      duration,
+      panelClass: ['myapp-danger-background-snackbar']
+    });
+  }
 }

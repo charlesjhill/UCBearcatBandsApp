fe6d1756a120f053ae61de2ee244a07eadeb7631
Charles Hill <chasehill70@gmail.com>
2019-11-24 19:17:33 -0500

    Removed redundant list() from UserService
    List() returned an array of *Students*, which is weird since this service manages users, and we already have a StudentService, so I got rid of it.

--- AngularClient/src/app/_services/user.service.ts
+++ AngularClient/src/app/_services/user.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User, RegisterUser, TokenReturn } from '../_models';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
-
+import { Observable, BehaviorSubject } from 'rxjs';
 
 export interface Student {
   user: User;
@@ -13,17 +12,13 @@ export interface Student {
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    register(user: RegisterUser): Observable<TokenReturn> {
-        return this.http.post<TokenReturn>(`${environment.apiUrl}/rest-auth/registration/`, user);
-    }
-
-    update(user: User): Observable<User> {
-        return this.http.put<User>(`${environment.apiUrl}/rest-auth/user/`, user);
+  register(user: RegisterUser): Observable<TokenReturn> {
+    return this.http.post<TokenReturn>(`${environment.apiUrl}/rest-auth/registration/`, user);
   }
 
-  list(): Observable<Student[]> {
-    return this.http.get<Student[]>(`${environment.apiUrl}/students/`);
+  update(user: User): Observable<User> {
+    return this.http.put<User>(`${environment.apiUrl}/rest-auth/user/`, user);
   }
 }

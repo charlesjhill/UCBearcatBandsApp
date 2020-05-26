175ad6f8a0884d07d84ed0bb82f3622d13adafe3
Charles Hill <charles.hill@etegent.com>
2020-05-25 23:49:23 -0400

    The Ensemble Dashboard now retains the currently opened ensemble
    HOWEVER: There is a bug that if you open another ensemble without closing another one, the route parameter is set to none-selected, which isn't the desired behavior. This seems to be because the opened event of the new ensemble is fired _before_ the closed event of the previous ensemble, thus the final state is that the query is closed.
    
    When an ensemble is opened, its ID is added to the current URL as an optional route parameter. The component observes changes to this route parameter to expand one of the ensembles at a time.

--- AngularClient/src/app/dashboard-ensembles/dashboard-ensembles.component.ts
+++ AngularClient/src/app/dashboard-ensembles/dashboard-ensembles.component.ts
@@ -1,13 +1,13 @@
-import { AlertService } from './../_services/alert.service';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-import { Ensemble } from './../_models/Ensemble';
-import { EnsembleService } from './../_services/ensemble.service';
-import { Component, OnInit, Inject, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Subscription, Observable } from 'rxjs';
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { SnackBarService } from '../_services/snackbar.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Ensemble } from '../_models';
+import { AlertService, EnsembleService, SnackBarService } from '../_services';
+import { switchMap, map } from 'rxjs/operators';
 
 export interface DialogData {
   name: string;
@@ -20,40 +20,53 @@ export interface DialogData {
   templateUrl: './dashboard-ensembles.component.html',
   styleUrls: ['./dashboard-ensembles.component.css']
 })
-export class DashboardEnsemblesComponent implements OnInit, OnDestroy, AfterViewInit {
-  dataSource: MatTableDataSource<Ensemble>;
-  currentEnsemble: Ensemble;
-  ensSubscription: Subscription;
-  ensembles$: Observable<Ensemble[]>;
+export class DashboardEnsemblesComponent implements OnInit, OnDestroy {
+  public ensembles$: Observable<Ensemble[]>;
+  /** Tracks the currently open ensemble. */
+  public selectedEnsemble: number;
+
+  private currentEnsemble: Ensemble;
+  private dataSource: MatTableDataSource<Ensemble>;
+  private ensSubscription: Subscription;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private alertService: AlertService,
-              private ensembleService: EnsembleService,
               public dialog: MatDialog,
-              private snackBarService: SnackBarService) { }
+              private ensembleService: EnsembleService,
+              private snackBarService: SnackBarService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.route.paramMap.pipe(
+      map(params => Number(params.get('selectedEnsemble')))
+    ).subscribe(selectedEnsemble => this.selectedEnsemble = selectedEnsemble);
+
     // Get the ensembles from our ensembleService
     this.dataSource = new MatTableDataSource();
+    this.dataSource.paginator = this.paginator;
     this.ensSubscription = this.ensembleService.currentEnsembles.subscribe(d => {
       this.dataSource.data = d;
-      this.dataSource.paginator = this.paginator;
       this.ensembles$ = this.dataSource.connect();
     });
 
     this.currentEnsemble = new Ensemble();
   }
 
-  ngAfterViewInit() {
-    // this.ensSubscription = this.ensembleService.currentEnsembles.subscribe(d => {
-    //   this.dataSource.data = d;
-    // });
-  }
-
   ngOnDestroy(): void {
     if (this.dataSource) { this.dataSource.disconnect(); }
     if (this.ensSubscription) { this.ensSubscription.unsubscribe(); }
+  }
+
+  changeSelectedEnsemble(newId: number) {
+    if (newId === -1) {
+      this.router.navigate(['.', { }], { relativeTo: this.route});
+      return;
+    }
+    this.router.navigate(['.', { selectedEnsemble: newId }], {
+      relativeTo: this.route
+    });
   }
 
   addEnsemble() {
@@ -71,14 +84,14 @@ export class DashboardEnsemblesComponent implements OnInit, OnDestroy, AfterView
       console.log('Add Ensemble dialog closed');
       if (result) {
         this.ensembleService.add(result)
-        .subscribe(
-          data => {
-            this.snackBarService.openSnackBar('Ensemble Added');
-          }, error => {
-            this.alertService.error(error);
-          }
-        )
-        ;
+          .subscribe(
+            data => {
+              this.snackBarService.openSnackBar('Ensemble Added');
+            }, error => {
+              this.alertService.error(error);
+            }
+          )
+          ;
       }
     });
   }
@@ -94,8 +107,8 @@ export class DashEnsembleAddDialog implements OnInit {
   constructor(
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<DashEnsembleAddDialog>,
-    @Inject (MAT_DIALOG_DATA) public data: any
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -114,6 +127,4 @@ export class DashEnsembleAddDialog implements OnInit {
   exit() {
     this.dialogRef.close();
   }
-
-
 }

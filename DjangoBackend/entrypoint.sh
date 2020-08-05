e0d835b2007679d1fa979ae585f678ad9f13ae3c
Charles Hill <chasehill70@gmail.com>
2020-08-05 14:15:45 -0400

    Ensure migrations are made before starting up the server

--- DjangoBackend/entrypoint.sh
+++ DjangoBackend/entrypoint.sh
@@ -3,18 +3,16 @@
 if [ "$DATABASE" = "postgres" ]; then
     echo "Waiting for postgres..."
 
-    while ! nc -z $DATABSE_HOST $DATABASE_PORT; do
-        sleep 0.1
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+      sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
-echo "Making migrations and migrating the db"
-python manage.py makemigrations main --noinput
-python manage.py migrate --noinput
-
-# If we want to collect our static files, we can do that below
-# python manage.py collectstatic
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
+python manage.py makemigrations --noinput 
+python manage.py migrate --noinput 
 
 exec "$@"
